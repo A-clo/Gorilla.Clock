06b8c309cb1e05c3a7fcaced3d63fd665793222a
A-clo <65191457+A-clo@users.noreply.github.com>
2020-05-17 20:00:39 -0700

    Update Gorilla.py

--- Gorilla.py
+++ Gorilla.py
@@ -1,4 +1,82 @@
-    p
+
+import sys
+import time
+import subprocess
+
+WORK_MINUTES = 25
+BREAK_MINUTES = 5
+
+
+def main():
+    try:
+        if len(sys.argv) <= 1:
+            print(f'🦧 gorilla {WORK_MINUTES} minutes. Ctrl+C to exit')
+            gorilla(WORK_MINUTES, 'It is time to take a break')
+            print(f'🐉 break {BREAK_MINUTES} minutes. Ctrl+C to exit')
+            gorilla(BREAK_MINUTES, 'It is time to work')
+
+        elif sys.argv[1] == '-t':
+            minutes = int(sys.argv[2]) if len(sys.argv) > 2 else WORK_MINUTES
+            print(f'🦧 gorilla {minutes} minutes. Ctrl+C to exit')
+            gorilla(minutes, 'It is time to take a break')
+
+        elif sys.argv[1] == '-b':
+            minutes = int(sys.argv[2]) if len(sys.argv) > 2 else BREAK_MINUTES
+            print(f'🐉 break {minutes} minutes. Ctrl+C to exit')
+            gorilla(minutes, 'It is time to work')
+
+        elif sys.argv[1] == '-h':
+            help()
+
+        else:
+            help()
+
+    except KeyboardInterrupt:
+        print('\n👋 goodbye')
+    except Exception as ex:
+        print(ex)
+        exit(1)
+
+
+def gorilla(minutes, notify_msg):
+    start_time = time.perf_counter()
+    while True:
+        diff_seconds = int(round(time.perf_counter() - start_time))
+        left_seconds = minutes * 60 - diff_seconds
+        if left_seconds <= 0:
+            print('')
+            break
+
+        countdown = '{}:{} ⏰'.format(int(left_seconds / 60), int(left_seconds % 60))
+        duration = min(minutes, 25)
+        progressbar(diff_seconds, minutes * 60, duration, countdown)
+        time.sleep(1)
+
+    notify_me(notify_msg)
+
+
+def progressbar(curr, total, duration=10, extra=''):
+    frac = curr / total
+    filled = round(frac * duration)
+    print('\r', '🦧' * filled + '--' * (duration - filled), '[{:.0%}]'.format(frac), extra, end='')
+
+
+def notify_me(msg):
+    '''
+    # macos desktop notification
+    terminal-notifier -> https://github.com/julienXX/terminal-notifier#download
+    terminal-notifier -message <msg>
+    # ubuntu desktop notification
+    notify-send
+    # voice notification
+    say -v <lang> <msg>
+    lang options:
+    - Daniel:       British English
+    - Ting-Ting:    Mandarin
+    - Sin-ji:       Cantonese
+    '''
+
+    print(msg)
     try:
         if sys.platform == 'darwin':
             
